6e2010d170fbf2213f240059dfbc70556081e255
Koji Nakanishi <lazycozy@gmail.com>
2015-05-25 08:33:20 +0900

    Add tentative motions to confirm the key frames

---
+++ motions/test.sh
@@ -0,0 +1,7 @@
+./moplay ./motion-001.json
+./moplay ./motion-002.json
+./moplay ./motion-003.json
+./moplay ./motion-004.json
+./moplay ./motion-005.json
+./moplay ./motion-006.json
+./moplay ./motion-007.json
